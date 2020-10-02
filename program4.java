4ccd3a7d27d7c1e49eac68e4e1358b89aad7968a
Hima Wijesuriya <71540782+himaya123@users.noreply.github.com>
2020-10-02 22:09:18 +0530

    Create program4.java
    add new java file

---
+++ program4.java
@@ -0,0 +1,22 @@
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
+public class ArithmeticOperatorDemo {
+   public static void main(String args[]) {
+      int num1 = 100;
+      int num2 = 20;
+
+      System.out.println("num1 + num2: " + (num1 + num2) );
+      System.out.println("num1 - num2: " + (num1 - num2) );
+      System.out.println("num1 * num2: " + (num1 * num2) ); 
+      System.out.println("num1 / num2: " + (num1 / num2) );
+      System.out.println("num1 % num2: " + (num1 % num2) );
+   }
+}
